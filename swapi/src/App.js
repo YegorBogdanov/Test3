a121545bd8b71f42413110441ef1ae896f863cd9
YegorBogdanov <megappt@mail.ru>
2020-10-17 13:16:42 +0300

    swapi done

--- swapi/src/App.js
+++ swapi/src/App.js
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
 import CharacterContext from './contexts/CharacterContext';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
 import CharacterList from './components/CharacterList';
 import Character from './components/Character';
 
 function App() {
 
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
+  const [primaryData, setPrimaryData] = useState([])
+  const [input, setInput] = useState('');
+  const [error, setError] = useState('');
+  // const [filteredData, setFilteredData] = useState([]);
+  const history = useHistory();
+
   useEffect(() => {
     (async () => {
       const response = await fetch("https://swapi.dev/api/films/1/", {
@@ -16,10 +22,39 @@ function App() {
       const charactersURL = await response.json();
       const charactersResponses = await Promise.all(charactersURL.characters.map((character) => fetch(character)));
       const characters = await Promise.all(charactersResponses.map((character) => character.json()));
-      setData(characters)
-      // console.log(characters);
+      setPrimaryData(characters);
+      setData(primaryData)
     })()
   }, [])
+
+  function sortHeroesByHeight({ target: { value } }) {
+    console.log(value);
+    if (value === 'ascending') {
+      setData((prev) => [...prev].sort((a, b) => a.height - b.height))
+    } else {
+      setData((prev) => [...prev].sort((a, b) => b.height - a.height))
+    }
+    console.log(data);
+  };
+
+  function genderFilter({ target: { value } }) {
+    const filtered = primaryData.filter(({ gender }) => gender === value)
+    setData((prev) => filtered)
+  }
+
+  function findHero(event) {
+    event.preventDefault();
+    const hero = data.find(({ name }) => name === input)
+    if (hero) {
+      history.push(`/characters/${hero.name}`)
+    } else {
+      setError("wrong name")
+    }
+  }
+
+  function change({ target: { value } }) {
+    setInput(value)
+  }
 
   return (
     <CharacterContext.Provider value={{ data, setData }}>
@@ -27,14 +62,32 @@ function App() {
         <Route path="/characters/:characterName">
           <Character />
         </Route>
+        <div className="App">
+          <header className="App-header">
+            <div>
+              <form onSubmit={findHero}>
+                {error && <p>{error}</p>}
+                <label>find hero by name</label> {' '}
+                <input onChange={change} name='name'></input>
+                <button type="submit">Find</button>
+                <br />
+                <label>Height sort</label> {' '}
+                <select onChange={sortHeroesByHeight}>
+                  <option value="ascending">ascending</option>
+                  <option value="descending">descending</option>
+                </select>
+                <br />
+                <label>Gender filter</label> {' '}
+                <select onChange={genderFilter}>
+                  <option value="male">male</option>
+                  <option value="female">female</option>
+                </select>
+              </form>
+              <CharacterList />
+            </div>
+          </header>
+        </div>
       </Switch>
-      <div className="App">
-        <header className="App-header">
-          <div>
-            <CharacterList />
-          </div>
-        </header>
-      </div>
     </CharacterContext.Provider>
   );
 }
